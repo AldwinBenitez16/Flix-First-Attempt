790ea6168ca7cda561c2a1ac5acfa5d4c73c0447
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-15 23:14:12 -0400

    Updates

---
+++ precache-manifest.b223162c2fde2f85fe1136fed607f22a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078785c92ff30bed98ecd0a3c195ba0c",
+    "url": "/Flix/index.html"
+  },
+  {
+    "revision": "aa6fac7d4e5a0588b938",
+    "url": "/Flix/static/css/2.2825a612.chunk.css"
+  },
+  {
+    "revision": "249557cab2496d9b2b5a",
+    "url": "/Flix/static/css/main.e097a6f4.chunk.css"
+  },
+  {
+    "revision": "aa6fac7d4e5a0588b938",
+    "url": "/Flix/static/js/2.f124478e.chunk.js"
+  },
+  {
+    "revision": "249557cab2496d9b2b5a",
+    "url": "/Flix/static/js/main.c3b12462.chunk.js"
+  },
+  {
+    "revision": "ca7254014fa292971501",
+    "url": "/Flix/static/js/runtime~main.cc984cc0.js"
+  },
+  {
+    "revision": "b86d93d8d8794f9d273ed913e0736037",
+    "url": "/Flix/static/media/add-favorite.b86d93d8.svg"
+  },
+  {
+    "revision": "37f140af0660610940052dd82fd5c6e5",
+    "url": "/Flix/static/media/add-watch.37f140af.svg"
+  },
+  {
+    "revision": "058c0cd2de61283ae737c2b9d20e213b",
+    "url": "/Flix/static/media/addlist.058c0cd2.svg"
+  },
+  {
+    "revision": "4f3b8db783482bda61a8c2bbfeea5b4e",
+    "url": "/Flix/static/media/github.4f3b8db7.svg"
+  },
+  {
+    "revision": "62ff70eaf24bcbf6e067c08861a65cd8",
+    "url": "/Flix/static/media/home.62ff70ea.svg"
+  },
+  {
+    "revision": "3bc4851064c99709055308adc5bfd160",
+    "url": "/Flix/static/media/linkedin.3bc48510.svg"
+  },
+  {
+    "revision": "d8d02db37c962c37a293bcec384ed8bc",
+    "url": "/Flix/static/media/list.d8d02db3.svg"
+  },
+  {
+    "revision": "266b3eae7e864608a2fda1a5aff48be0",
+    "url": "/Flix/static/media/logo.266b3eae.svg"
+  },
+  {
+    "revision": "6583d48c8b8384a3fb4772ecb1fc1da9",
+    "url": "/Flix/static/media/mail.6583d48c.svg"
+  },
+  {
+    "revision": "29f2a97f8ebf14e8ea7ff1b1d2457871",
+    "url": "/Flix/static/media/remove-favorite.29f2a97f.svg"
+  },
+  {
+    "revision": "2391d00f0dcfdc087c8f7e15d9eece91",
+    "url": "/Flix/static/media/remove-watch.2391d00f.svg"
+  },
+  {
+    "revision": "cd059951372af4d03cd6240c7e72ebe2",
+    "url": "/Flix/static/media/removelist.cd059951.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Flix/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Flix/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Flix/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Flix/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Flix from './containers/Flix';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Flix />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
